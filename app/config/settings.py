60785d69484de7cf10a6d6cf39670cf6ceadacb5
Rama Prasad Das <rama.idmail@gmail.com>
2025-05-25 20:52:26 +0530

    configurations file updated

--- app/config/settings.py
+++ app/config/settings.py
@@ -5,8 +5,8 @@ class Settings(BaseSettings):
     GEMINI_API_TOKEN: str
     DISCORD_BOT_TOKEN: str
 
-    REDIS_HOST: str
-    REDIS_PORT: int 
+    REDIS_HOST: str 
+    REDIS_PORT: int = 6379
 
     class config:
         env_file = ".env"
