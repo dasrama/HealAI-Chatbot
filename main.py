d0594b0b5efa8da57052ad2d420d99a062681dae
Rama Prasad Das <rama.idmail@gmail.com>
2025-10-29 08:35:43 +0530

    refactor: update docker-compose configuration and clean up main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import os
-from bot.commands import bot   # assuming your bot code is in app/bot.py
+from bot.commands import bot   
 from config.settings import Settings
 
 if __name__ == "__main__":
-    # Get Discord token from env (recommended)
     TOKEN = Settings().DISCORD_BOT_TOKEN
 
     if not TOKEN:
